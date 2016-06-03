feb655f6ebd74fa778347a442723745bf1c1b541
likun01 <likun01@harvestwm.cn>
2016-06-03 11:32:10 +0800

    tweak

--- bigvs/urls.py
+++ bigvs/urls.py
@@ -5,6 +5,8 @@ Created on 2016年4月29日
 @author: likun
 '''
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 from bigvs.views import BigVsListView, BigVsIndexView, follow, unfollow, \
     BigvDetailView, BigvJsonDataForTitleView
 from common.decorators import openid_exempt
@@ -16,6 +18,6 @@ urlpatterns = [
     url(r'^wechat/follow/$', follow, name='follow'),
     url(r'^wechat/unfollow/$', unfollow, name='unfollow'),
     url(r'^jsondata_for_title/$', openid_exempt(BigvJsonDataForTitleView.as_view()), name='bigvs_jsondata_for_title'),
-    url(r'^(?P<v_id>[\w-]+)/$', BigvDetailView.as_view(), name='bigvs_detail'),
+    url(r'^(?P<v_id>[\w-]+)/$', cache_page(settings.SITE_REDIS_TIMEOUT)(BigvDetailView.as_view()), name='bigvs_detail'),
 ]
 
